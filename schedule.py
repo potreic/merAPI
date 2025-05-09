f3348300089117ed9ca75392757cdd78b5136996
potreic <nibroosauroremajiidharyanto@mail.ugm.ac.id>
2025-05-10 02:37:06 +0800

    feat: cleaning repo

--- schedule.py
+++
@@ -1,8 +0,0 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-from raw import load_and_save_raw
-from transform import transform_merapi
-
-sched = BlockingScheduler()
-sched.add_job(load_and_save_raw,  'cron', minute='0')   # setiap jam
-sched.add_job(transform_merapi,   'cron', minute='5')   # jam +5 menit
-sched.start()
